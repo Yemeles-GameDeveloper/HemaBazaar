184570c03d7c41a80e7049835223fd21a0d3dc76
Yemeles-GameDeveloper <yazilimogreniyoreren@gmail.com>
2025-11-27 23:03:17 +0300

    MVC Start

---
+++ HemaBazaar.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HemaBazaar.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

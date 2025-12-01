9e9a25f75cfdb6b7d6c73a3374d11fc126e66da9
Yemeles-GameDeveloper <yazilimogreniyoreren@gmail.com>
2025-12-01 23:22:03 +0300

    HTML Template and AccountController

---
+++ HemaBazaar.MVC/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HemaBazaar.MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
+        //18 Kasım 2:36:00 Index cshtml e login ve register kısmı ekleyeceksin unutma.
+    }
+}

fc3a17bd24ce53b36986581174f3fe0ee9f02635
Yemeles-GameDeveloper <yazilimogreniyoreren@gmail.com>
2025-12-04 00:02:45 +0300

    Cart and Item views

---
+++ HemaBazaar.MVC/Controllers/ItemController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HemaBazaar.MVC.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
